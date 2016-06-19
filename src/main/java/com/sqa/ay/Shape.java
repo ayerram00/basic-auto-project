7d7b2a530715359a9112bb06ab14ec410703ec9c
Anand Yerramsetty <anand.shopping@gmail.com>
2016-06-18 17:18:55 -0700

    update src files

---
+++ src/main/java/com/sqa/ay/Shape.java
@@ -0,0 +1,29 @@
+/**
+ *   File Name: Shape.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 18, 2016
+ *   
+ */
+
+package com.sqa.ay;
+
+/**
+ * Shape //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public enum Shape {
+
+	CIRCLE, RECTANGLE, SQUARE
+
+}
